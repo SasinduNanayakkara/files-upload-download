1a9172c91c1c84bf352faf0837aa6ae3fe96511e
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-02-16 23:54:33 +0530

    create form and upload function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import React, { useState } from "react";
+import { app } from "./base";
+import { storage} from "./base"
 
 function App() {
+
+  const [progess, setProgress] = useState(0);
+  const onFileChange = (file) => {
+      // const file = e.target.files[0]
+      // const storageRef = app.storage().ref()
+      // const fileRef = storageRef.child(file.name)
+      // fileRef.put(file).then(() => {
+      //   console.log("Uploaded file", file.name)
+      // });
+      if (!file) return;
+      const storageRef = ref(storage, `/files/${file.name}`);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on("state_changed", (snapshot) => {
+        const prog = Math.round(
+          (snapshot.bytesTransferred/ snapshot.totalBytes) * 100
+        );
+
+        setProgress(prog);
+      }, (err) => 
+        console.log(err),
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(url => console.log(url));
+        }
+      );
+  }
+
+  const onSubmit = (e) => {
+      e.preventDefault();
+      const file = e.target[0].files[0];
+      onFileChange(file);
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={onSubmit}>
+        <input type="file"/>
+        <input type="text" name="username" placeholder="NAME"/>
+        <button type="submit">Submit</button>
+      </form>
+      <hr/>
+      <h3>Uploaded {progess} %</h3>
+      <ul>
+        <li>---</li>
+      </ul>
+    </>
   );
 }
 
